1a10a35c85e84a23f0cd65d0c4c3cec3003ad2b8
Devanshukoli <kolidevanshu02@gmail.com>
2023-08-23 22:14:08 +0530

    <add> : Solved question in class22-materail/space-review file

--- Class 22/class22-materials/space-review/js/main.js
+++ Class 22/class22-materials/space-review/js/main.js
@@ -1,12 +1,42 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
+let arrNum = [1, 2, 3, 4, 5]
+let sum = 0
+for (let i = 0; i < arrNum.length; i++) {
+    sum += arrNum[i];
+}
+console.log(sum);
+// alert(sum);
+
+
 
 //Create a function that takes in an array of numbers
-//Return a new array of numbers that is every original number squared
+function square(arr) {
+    //Return a new array of numbers that is every original number squared
+    return arr.map(x => x * x)
+}
+
+console.log(square(arrNum));
 
 //Create a function that takes string
-//Print the reverse of that string to the console
+function reverseStr(str) {
+    //Print the reverse of that string to the console
+    return str.split("").reverse().join('')
+    
+}
+console.log(reverseStr("hello"));
 
 //Create a function that takes in a string
-//Alert if the string is a palindrome or not
+function checkPalindrome(str) {
+    //Alert if the string is a palindrome or not
+    if (str === str.split('').reverse().join('')) {
+        console.log('Yes')
+        // alert('Yes')
+    } else {
+        console.log('No')
+        // alert('No')
+    }
+}
+
+checkPalindrome('racecar')
