3ad26247a17392bc02dff6601c9b9d71262aaaad
Devanshukoli <kolidevanshu02@gmail.com>
2023-08-24 10:54:56 +0530

    <add> : Solved problem for class22-materials/objects-lost-galaxy

--- Class 22/class22-materials/objects-lost-galaxy/js/main.js
+++ Class 22/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,20 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse = {}
+
+mouse.color = 'black'
+mouse.size = 'small'
+mouse.tail = 'long'
+mouse.ears = 'big'
+
+mouse.run = function () {
+    console.log('run')
+}
+
+mouse.eat = function () {
+    console.log('eat')
+}
+
+mouse.sleep = function () {
+    console.log('sleep')
+}
