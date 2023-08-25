cd63f3d56f81797794acb36f2937d0a4a831a5cc
Devanshukoli <kolidevanshu02@gmail.com>
2023-08-25 14:39:01 +0530

    <update>: Use constructor method in class23 material/objects-lost-galaxy

--- Class 23/class23-materials/objects-lost-galaxy/js/main.js
+++ Class 23/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
 //Create a dog object that has four properties and three methods
+// var dog = {}
+ 
+// dog.name = "Fido";
+// dog.breed = "Lab";
+// dog.age = 3;
+// dog.color = "Black";
+
+// dog.bark = () => {console.log('bark')}
+// dog.jump = () => {console.log('jump')}
+// dog.run = () => { console.log('run') }
+
+// Using Constructor function.
+function Dog(name, breed, age, color) {
+    this.name = 'Fido'
+    this.breed = 'Lab'
+    this.age = 3
+    this.color = 'Black'
+
+    this.bark = () => { console.log('bark') }
+    this.jump = () => { console.log('jump') }
+    this.run = () => { console.log('run') }
+}
+
+var germanSheford = new Dog('German Sheford', 'German Sheford', 5, 'Brown')
