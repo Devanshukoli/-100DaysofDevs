d62d704f2ba7c1ba8cd59dd6c9bd969ff4e7affa
Devanshukoli <kolidevanshu02@gmail.com>
2024-05-08 18:30:36 +0530

    [chore] Add code for local-storage working.

--- Class 27/class27-materials/local-storage-botScore-button/js/main.js
+++ Class 27/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
 
+if (!localStorage.getItem('botScore')) {
+    localStorage.setItem('botScore', 0)
+}
 
+document.querySelector('button').addEventListener('click', addAnotherOne)
+
+function addAnotherOne() {
+    let botScroeVal = Number(localStorage.getItem('botScore'))
+    botScroeVal += 1
+    localStorage.setItem('botScore', botScroeVal)
+}
